036edb4609182f5ab5ea45d68c901202da1b662a
Klitix <54744565+Klitix@users.noreply.github.com>
2025-09-13 20:25:40 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
-int main() {
-  std::cout << 700 << std::endl;
-  return 0;
+int main()
+{
+    int result = 0;
+    int n;
+  
+    std::cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        int counts;
+        int cost;
+        
+        std::cin >> counts >> cost;
+        result = result + (counts * cost);
+    }
+    
+    std::cout << result;
+    
+    return 0;
 }
