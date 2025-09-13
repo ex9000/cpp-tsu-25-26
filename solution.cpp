4f2c6d0f8078cc45e930ed9ac083157e6cf22a4c
Andrey-Gasilow-std <babaliga9191@inbox.ru>
2025-09-13 19:19:13 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+using namespace std;
+
+
 int main() {
-  std::cout << 700 << std::endl;
-  return 0;
+    
+    int n;
+    cout << "Кол-во видов товара: ";
+    cin >> n;
+    
+    if (n <= 0) {
+        cout << "Ничего не куплено. Стоимость чека: 0" << endl;
+        return 0;
+    }
+    
+ int amount = 0;
+
+ for(int i = 1; i <= n; i++){
+     int price;
+     int amt;
+     
+     cout << "Кол-во товаров №:" << i << ": ";
+     cin >> amt;
+     
+     cout << "Цена товара №:" << i << ": ";
+     cin >> price;
+     
+     amount += amt * price;
+ }
+ 
+ cout << "Cтоимость чека: " << amount << endl;
+ 
+ return 0;
+
 }
