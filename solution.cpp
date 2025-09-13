b767440a3199e316135af8d1cd2def139acd9849
Andrey-Gasilow-std <babaliga9191@inbox.ru>
2025-09-13 19:32:02 +0300

    Update norm solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,28 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    
-  int n;
-  cin >> n;
-  
-  int amount = 0;
 
-  for(int i = 1; i <= n; i++){
-      int price;
-      int amt;
-     
-      cout << i ;
-      cin >> amt;
-     
-      cout << i;
-      cin >> price;
-     
-      amount += amt * price;
-   }
- 
-  cout << amount << endl;
- 
-  return 0;
+    int n;
+    cin >> n;
+  
+    int amount = 0;
+
+    for(int i = 1; i <= n; i++){
+        int price;
+        int amt;
+        ;
+        cin >> amt;
+        cin >> price;
+        amount += amt * price;
+        
+    }
+
+    cout << amount << endl;
+    return 0;
 
 }
