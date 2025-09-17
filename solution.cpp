6cc72689d8ff4681b6a69fc35a2e3a2b4a516a5e
n45567 <nea_30@mail.ru>
2025-09-17 15:32:42 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,12 +3,12 @@
 int main() {
   int n;
   std::cin >> n;
-  int total = 0
+  int total = 0;
   
   for (int i = 0; i < n; i++) {
     int k, p;
     std::cin >> k >> p;
-    total += k * p
+    total += k * p;
   }
   
   std::cout << total << std::endl;
