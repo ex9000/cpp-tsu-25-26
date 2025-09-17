e8c2d3af0594075a8bb45ea102bc08a3b9ef24f5
n45567 <nea_30@mail.ru>
2025-09-17 20:33:28 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,7 +3,6 @@
 
 int main() {
     std::string line;
-    // Считаем первую строку и извлечём n
     std::getline(std::cin, line);
     int pos = line.rfind(' ');
     int n = std::stoi(line.substr(pos + 1));
@@ -16,28 +15,21 @@ int main() {
 
         int k = 1;
         int p = 0;
-
-        // Ищем " за " или " по "
         std::size_t pos_za = line.find(" за ");
         std::size_t pos_po = line.find(" по ");
 
         if (pos_za != std::string::npos) {
-            // Формат: ... за p рублей
             std::size_t start = pos_za + 4;
-            std::size_t end = line.find(' ', start);
-            std::string num = line.substr(start, end - start);
+            std::string num = line.substr(start);
             p = std::stoi(num);
         } else if (pos_po != std::string::npos) {
-            // Формат: ... по p рублей: k штуки
             std::size_t start_p = pos_po + 4;
-            std::size_t end_p = line.find(' ', start_p);
-            std::string num_p = line.substr(start_p, end_p - start_p);
+            std::string num_p = line.substr(start_p);
             p = std::stoi(num_p);
 
-            std::size_t colon = line.find(':', end_p);
+            std::size_t colon = line.find(':', start_p);
             std::size_t start_k = colon + 2; // после ": "
-            std::size_t end_k = line.find(' ', start_k);
-            std::string num_k = line.substr(start_k, end_k - start_k);
+            std::string num_k = line.substr(start_k);
             k = std::stoi(num_k);
         }
 
