fea90ed3f531b8c76c0d0efea361bf67b86e04d6
n45567 <nea_30@mail.ru>
2025-09-17 15:17:55 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 int main() {
+  int n
+  std::cin >> n;
+  int total = 0
+  for (int i = 0; i < n; i++) {
+    int k, p;
+    std::cin >> k >> p;
+    total += k * p
+  }
   std::cout << 700 << std::endl;
   return 0;
 }
