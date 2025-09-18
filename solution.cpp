0a8d0c64018a1e5a46ab2967ec591de3e4540a4c
n45567 <nea_30@mail.ru>
2025-09-18 19:21:20 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -2,15 +2,12 @@
 #include <string>
 int main() {
     std::string line;
-    std::getline(std::cin, line);
     int total_sum = 0;
     int total_k = 0;
     for (int i = 0; i < n; ++i) {
         std::getline(std::cin, line);
         int k = 1;
         int p = 0;
-        std::size_t pos_za = line.find(" за ");
-        std::size_t pos_po = line.find(" по ");
         total_sum += k * p;
         total_k += k;
     }
