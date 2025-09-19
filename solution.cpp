e474e78073b6379a323df6ba459186b55a3359af
not3vial <159703900+not3vial@users.noreply.github.com>
2025-09-19 21:57:49 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
+#include <iostream>
 
 int main()
 {
     int n;
     std::cin >> n;
-    int c, p, s;
+    int c, p;
+    int s = 0;
     for(int i = 0; i<n; i++) {
         std::cin >> c >> p;
-        s = s + c * p;
+        s += c * p;
     }
     std::cout << s << std::endl;
+    return 0;
 }
