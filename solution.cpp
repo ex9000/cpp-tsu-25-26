1705e238cbfc9096bfdcb3a8e53f62f2d0dba319
not3vial <159703900+not3vial@users.noreply.github.com>
2025-09-19 22:36:02 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
-int main() {
-  std::cout << "Куплен товар за 100 рублей" << std::endl;
-  return 0;
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s >> s >> s;
+    int n;
+    cin >> n;
+    int k = 0;
+    int sum = 0;
+    int c, p;
+    int i = 0;
+    while(i < n) {
+        cin >> s;
+        if(s == "за") {
+            cin >> p;
+            k++;
+            sum += p;
+            i++;
+        }
+        if(s == "по") {
+            cin >> p >> s >> c;
+            k += c;
+            sum += c * p;
+            i++;
+        }
+    }
+    if(k == 1) {
+        cout << "Куплен товар за " << sum << " рублей" << endl;
+    }
+    else {
+        cout << "Куплено " << k << " штук товара за " << sum << " рублей" << endl;
+    }
+    return 0;
 }
