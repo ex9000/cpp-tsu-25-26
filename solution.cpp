dca2c7f5b8c9ae740be77397f660783e3909f52b
zoya-chizhova <chizhovazoya06@gmail.com>
2025-09-19 21:47:55 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -2,14 +2,14 @@
 int main() {
   int n;
   int sum;
-  std::cin>>n;
-  for(int q=0;q<n;q++){
+  std::cin >> n;
+  for (int q=0;q<n;q++) {
   int number;
   int price;
-  std::cin>>number;
-  std::cin>>price;
-  sum+=number*price;
+  std::cin >> number;
+  std::cin >> price;
+  sum += number*price;
   }
-  std::cout<<sum << std::endl;
+  std::cout << sum << std::endl;
   return 0;
 }
