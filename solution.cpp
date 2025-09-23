af6ccbd10816a88c48d99e5f1d55dec95e293a0f
n45567 <nea_30@mail.ru>
2025-09-23 10:24:18 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -2,24 +2,27 @@
 #include <string>
 int main() {
   int n;
-  std::cin >> "Позиций в чеке: " >> n;
+  std::string w1, w2, w3;
+  std::cin >> w1 >> w2 >> w3 >> n;
   int total = 0;
   int count = 0;
   for(int i; i < n, i++) {
     int k, p;
-    std::string b;
+    std::string w1, w2, w3;
+    std::cin >> w1 >> w2 >> w3;
+    std::getline(std::cin, line);
     if (n == 1) {
-      std::cin >> b >> " за " >> p >> " рублей";
+      std::cin >> line >> w1 >> p >> w2;
     } else {
-      std::cin >> b >> " по " >> p >> " рублей " >> k >> " штуки";
+      std::cin >> line >> w1 >> p >> w2 >> k >> w3;
     }
     total += k * p;
     count += k;
   }
   if (n == 1) {
-    std::cout << "Куплен товар за " << total << " рублей" << endl;
+    std::cout << w1 << total << w2 << endl;
   } else {
-    std::cout << "Куплено " << count << " штук товара за " << total << " рублей" << endl;
+    std::cout << w1 << count << w2 << total << w3 << endl;
   }
   return 0;
 }
