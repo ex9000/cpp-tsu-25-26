8b57a031a587a2f6742b528674724fc7fc6657f7
n45567 <nea_30@mail.ru>
2025-09-23 15:09:31 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,44 +1,26 @@
 #include <iostream>
 #include <string>
 int main() {
-    std::string w1, w2, w3;
     int n;
+    std::string w1, w2, w3;
     std::cin >> w1 >> w2 >> w3 >> n;
-    int total_sum = 0;
-    int total_count = 0;
-    std::string next_word; 
-    bool has_next_word = false;
-    for (int i = 0; i < n; ++i) {
-        int quantity = 1;
-        int price = 0;
-        std::string word;
-        if (has_next_word) {
-            word = next_word;
-            has_next_word = false;
+    int total = 0;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        int k, p;
+        std::string w1, w2, w3;
+        if (k == 1) {
+            std::cin >> w1 >> p >> w2;
         } else {
-            std::cin >> word;
+            std::cin >> w1 >> p >> w2 >> k >> w3;
         }
-        while (word != "за" && word != "по") {
-            std::cin >> word;
-        }
-        std::cin >> price;
-        std::cin >> word; // слово "рублей"
-        if (std::cin >> word) {
-            if (word == ":") {
-                std::cin >> quantity >> word; // читаем количество и "штуки"
-            } else {
-                next_word = word;
-                has_next_word = true;
-            }
-        }
-        total_sum += price * quantity;
-        total_count += quantity;
+        total += k * p;
+        count += k;
     }
-    if (total_count == 1) {
-        std::cout << "Куплен товар за " << total_sum << " рублей" << std::endl;
+    if (n == 1) {
+        std::cout << "Куплен товар за " << total << " рублей" << std::endl;
     } else {
-        std::cout << "Куплено " << total_count << " штук товара за " << total_sum << " рублей" << std::endl;
+        std::cout << "Куплено " << count << "штук товара за " << total << " рублей" << std::endl;
     }
-
-    return 0;
+        return 0;
 }
