4c325d79401cbfa1b343ec854f2c187ab4de5cdf
n45567 <nea_30@mail.ru>
2025-09-23 15:07:09 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,23 +3,32 @@
 int main() {
     std::string w1, w2, w3;
     int n;
-    std::cin >> w1 >> w2 >> w3 >> n; 
+    std::cin >> w1 >> w2 >> w3 >> n;
     int total_sum = 0;
     int total_count = 0;
+    std::string next_word; 
+    bool has_next_word = false;
     for (int i = 0; i < n; ++i) {
         int quantity = 1;
         int price = 0;
         std::string word;
-        while (true) {
+        if (has_next_word) {
+            word = next_word;
+            has_next_word = false;
+        } else {
             std::cin >> word;
-            if (word == "за" || word == "по") break;
         }
-        std::cin >> price;   
-        std::cin >> word;    
+        while (word != "за" && word != "по") {
+            std::cin >> word;
+        }
+        std::cin >> price;
+        std::cin >> word; // слово "рублей"
         if (std::cin >> word) {
             if (word == ":") {
-                std::cin >> quantity >> word;
+                std::cin >> quantity >> word; // читаем количество и "штуки"
             } else {
+                next_word = word;
+                has_next_word = true;
             }
         }
         total_sum += price * quantity;
@@ -30,5 +39,6 @@ int main() {
     } else {
         std::cout << "Куплено " << total_count << " штук товара за " << total_sum << " рублей" << std::endl;
     }
+
     return 0;
 }
