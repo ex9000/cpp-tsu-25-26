035f8aac89201816997474049c350b5bf3b0fdbb
n45567 <nea_30@mail.ru>
2025-09-23 14:10:26 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -18,7 +18,7 @@ int main() {
     count += k;
     }
     if (n == 1) {
-        std::cout << "Куплен товар за " << k << " рублей" << std::endl;
+        std::cout << "Куплен товар за " << total << " рублей" << std::endl;
     } else {
         std::cout << "Куплено " << count << "штук товара за " << total << " рублей" << std::endl;
     }
