3b680364859012a3a5b70a4ddc0fe6cc09041c56
n45567 <nea_30@mail.ru>
2025-09-23 10:37:58 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -2,14 +2,14 @@
 #include <string>
 int main() {
   int n;
-  std::string w1, w2, w3;
-  std::cin >> w1 >> w2 >> w3 >> n;
+  std::string w1, w2, w3, w7, w8, w9;
+  std::cin >> w1 >> w2 >> w3 >> w7 >> w8 >> w9 >> n;
   int total = 0;
   int count = 0;
   for(int i; i < n; i++) {
     int k, p;
-    std::string w1, w2, w3, line;
-    std::cin >> w1 >> w2 >> w3;
+    std::string w4, w5, w6, line;
+    std::cin >> w4 >> w5 >> w6;
     std::getline(std::cin, line);
     if (n == 1) {
       std::cin >> line >> w1 >> p >> w2;
@@ -20,9 +20,9 @@ int main() {
     count += k;
   }
   if (n == 1) {
-    std::cout << w1 << total << w2 << std::endl;
+    std::cout << w7 << total << w8 << std::endl;
   } else {
-    std::cout << w1 << count << w2 << total << w3 << std::endl;
+    std::cout << w7 << count << w8 << total << w9 << std::endl;
   }
   return 0;
 }
