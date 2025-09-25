433edbecee8d66d458aded9d98a3b49d67c0a58d
zoya-chizhova <chizhovazoya06@gmail.com>
2025-09-25 21:49:27 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,46 @@
+
+#include <string>
 #include <iostream>
-int main() {
-  std::cout << "Куплен товар за 100 рублей" << std::endl;
-  return 0;
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    cin >> s;
+    cin >> s;
+    
+    int n;
+    cin >> n;
+    
+    int goods = 0;
+    int sum = 0;
+    
+    for(int q=0;q<n;q++){
+    int number;
+    int price;
+    string word;
+    
+    while(word != "за" and word != "по"){
+        cin >> word;
+    }
+    if(word == "за"){
+        number = 1;
+        cin >> price;
+        cin >> word;
+    }else{
+        cin >> price;
+        cin >> word;
+        cin >> number;
+        cin >> word;
+    }
+    sum += number*price;
+    goods += number;
+    }
+    if(goods == 1){
+        cout<<"Куплен товар за "<<sum<<" рублей"<<endl;
+    }else{
+        cout<<"Куплено "<<goods<<" штук товара за "<<sum<<" рублей"<<endl;
+    }
+
+    return 0;
 }
