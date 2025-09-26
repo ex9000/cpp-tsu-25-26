6667df66c5c409c6ceda439975056d0f56c4dbf7
lelouch865 <popovegpops865@gmail.com>
2025-09-26 17:50:44 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <string>
 int main() {
-  string first_line;
-  cin >> first_line; 
-  cin >> first_line;
-  cin >> first_line; 
+  std::string first_line;
+  std::cin >> first_line; 
+  std::cin >> first_line;
+  std::cin >> first_line; 
   int n;
-  cin >> n; 
+  std::cin >> n; 
   int total_items = 0;
   int total_price = 0;
   for (int i = 0; i < n; i++) {
-      string word;
+      std::string word;
       int quantity = 1;
       int price = 0;
-      while (cin >> word) {
+      while (std::cin >> word) {
           if (word == "за" || word == "по") {
               break;
           }
       }
-      cin >> price;
+      std::cin >> price;
       if (word == "по") {
-          cin >> word; 
-          cin >> quantity; 
-          cin >> word;
+          std::cin >> word; 
+          std::cin >> quantity; 
+          std::cin >> word;
       }
       total_items += quantity;
       total_price += quantity * price;
   }
   if (total_items == 1) {
-      cout << "Куплен единственный товар за " << total_price << " рублей" << endl;
+      std::cout << "Куплен единственный товар за " << total_price << " рублей" << std::endl;
   } 
   else {
-      cout << "Куплено " << total_items << " штук товара за " << total_price << " рублей" << endl;
+      std::cout << "Куплено " << total_items << " штук товара за " << total_price << " рублей" << std::endl;
   } 
   return 0;
 }
