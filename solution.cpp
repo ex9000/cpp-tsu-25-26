40a5fc76b8fbd30942d07d549f90fd066c1687a0
UnkindledAsh501 <jedisinobi@gmail.com>
2025-09-26 21:16:01 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
 int main() {
-  std::cout << "Куплен товар за 100 рублей" << std::endl;
-  return 0;
+    int n;
+    cin >> n;
+    double total = 0;
+    int total_items = 0;
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        int k;
+        double p;
+
+        cin >> s >> k >> s >> p >> s;
+
+        total += k * p;
+        total_items += k;
+
+        if (k == 1) {
+            cout << "Куплен товар за " << p << " рублей" << endl;
+        } else {
+            cout << "Куплен товар в количестве " << k << " штук за " << p * k << " рублей" << endl;
+        }
+    }
+
+    cout << "Куплено " << total_items << " штук товара за " << total << " рублей" << endl;
+
+    return 0;
 }
+
