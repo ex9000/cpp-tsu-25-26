da1cbf75b231ad1e7be998cb440772519310fad1
victimuss <sattarovttek@gmail.com>
2025-09-26 12:43:05 +0200

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,11 +3,11 @@
 #include <string>
 using namespace std;
 
-void bubblesort(long long int arr[], int n){
+void bubblesort(long long arr[], int n){
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
+                long long temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
@@ -19,12 +19,12 @@ int main() {
     int n;
     cin >> n; 
     
-    int arr[n];
+    long long arr[n];
     
     for (int i = 0; i < n; i++) {
         string input;
         getline(cin >> ws, input);  
-        long long int value_to_add = stoi(input.substr(input.find_last_of(' ') + 1));
+        long long int value_to_add = stoll(input.substr(input.find_last_of(' ') + 1));
         arr[i] = value_to_add;
     }
     
