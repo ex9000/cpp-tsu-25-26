fa97f6a692af923c573ab3fef71cd9821e67bcf5
lelouch865 <popovegpops865@gmail.com>
2025-09-26 17:45:57 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
 #include <string>
 int main() {
-  int main() {
-    string first_line;
-    cin >> first_line; 
-    cin >> first_line;
-    cin >> first_line; 
-    int n;
-    cin >> n; 
-    int total_items = 0;
-    int total_price = 0;
-    for (int i = 0; i < n; i++) {
-        string word;
-        int quantity = 1;
-        int price = 0;
-        while (cin >> word) {
-            if (word == "за" || word == "по") {
-                break;
-            }
-        }
-        cin >> price;
-        if (word == "по") {
-            cin >> word; 
-            cin >> quantity; 
-            cin >> word;
-        }
-        total_items += quantity;
-        total_price += quantity * price;
-    }
-    if (total_items == 1) {
-        cout << "Куплен единственный товар за " << total_price << " рублей" << endl;
-    } 
-    else {
-        cout << "Куплено " << total_items << " штук товара за " << total_price << " рублей" << endl;
-    } 
-    return 0;
-}
+  string first_line;
+  cin >> first_line; 
+  cin >> first_line;
+  cin >> first_line; 
+  int n;
+  cin >> n; 
+  int total_items = 0;
+  int total_price = 0;
+  for (int i = 0; i < n; i++) {
+      string word;
+      int quantity = 1;
+      int price = 0;
+      while (cin >> word) {
+          if (word == "за" || word == "по") {
+              break;
+          }
+      }
+      cin >> price;
+      if (word == "по") {
+          cin >> word; 
+          cin >> quantity; 
+          cin >> word;
+      }
+      total_items += quantity;
+      total_price += quantity * price;
+  }
+  if (total_items == 1) {
+      cout << "Куплен единственный товар за " << total_price << " рублей" << endl;
+  } 
+  else {
+      cout << "Куплено " << total_items << " штук товара за " << total_price << " рублей" << endl;
+  } 
   return 0;
 }
+
