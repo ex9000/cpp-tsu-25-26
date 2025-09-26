e7c656ac7aedbb1c472086aeb1591256aa9ae0ba
UnkindledAsh501 <jedisinobi@gmail.com>
2025-09-26 21:32:07 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,35 +3,39 @@
 using namespace std;
 
 int main() {
+    string temp;
     int n;
-    cin >> n;
-    cin.ignore();
-
-    cout << "Позиций в чеке: " << n << endl;
-        
+    cin >> temp >> temp >> temp >> n;
+    
     int total_items = 0;
     int total_price = 0;
 
     for (int i = 0; i < n; i++) {
-        string name1, name2, name3, temp;
-        int k, price;
+        string word;
+        int k = 1;
+        int price;
 
-        cin >> name1;
-        cin >> name2;
-        cin >> name3;
-        cin >> temp;
-        cin >> price;
-        cin >> temp;
-
-        k = 1;
+        while (cin >> word) {
+            if (word == "за") {
+                cin >> price >> word;
+                k = 1;
+                break;
+            } else if (word == "по") {
+                cin >> price >> word;
+                cin >> k >> word;
+                break;
+            }
+        }
 
         total_items += k;
         total_price += k * price;
-
-        cout << name1 << " " << name2 << " " << name3 << " за " << price << " рублей" << endl;
     }
 
-    cout << "Куплено " << total_items << " штук товара за " << total_price << " рублей" << endl;
+    if (total_items == 1) {
+        cout << "Куплен товар за " << total_price << " рублей" << endl;
+    } else {
+        cout << "Куплено " << total_items << " штук товара за " << total_price << " рублей" << endl;
+    }
 
     return 0;
 }
