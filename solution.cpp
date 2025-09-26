da2a621d424730ca3ec1c7a435b5e34c4165c159
MrSh-code <ramilshigabutdinov1709@gmail.com>
2025-09-26 23:04:36 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
-int main() {
-  std::cout << 700 << std::endl;
-  return 0;
+#include<iostream>
+
+using namespace std;
+
+void billamount(int N){
+    int c=0,a,b;
+    for (int i=0;i<N;i++){
+        cin >> a >> b;
+        c+=a*b;
+    }
+    cout << c;
+}
+
+int main(){
+    int N;
+    cin >> N;
+    billamount(N);
 }
