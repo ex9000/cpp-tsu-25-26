55de2427f95a0df82ddb5c39f32fdbdb01d7fba3
MrSh-code <ramilshigabutdinov1709@gmail.com>
2025-09-26 23:31:38 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
-int main() {
-  std::cout << 700 << std::endl;
-  return 0;
+#include<iostream>
+
+using namespace std;
+
+int billamount(int N){
+    int c=0,a,b;
+    for (int i=0;i<N;i++){
+        cin >> a >> b;
+        c+=a*b;
+    }
+    return c;
+}
+
+int main(){
+    int N;
+    cin >> N;
+    cout << billamount(N);
 }
