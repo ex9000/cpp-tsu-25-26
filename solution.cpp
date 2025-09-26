007b4a0d16c9656de2e708ff00df3902b9c90eaf
NoNegative64 <sashaevstratov52@mail.ru>
2025-09-26 23:07:17 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+
 int main() {
-  std::cout << 700 << std::endl;
+  int N;
+  std::cin>>N;
+  int col1,price1,col2,price2,col3,price3;
+  std::cin>>col1>>price1>>col2>>price2>>col3>>price3;
+  int sum = (col1*price1) + (col2*price2) + (col3*price3);
+  std::cout<<sum;
   return 0;
 }
