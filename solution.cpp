9c96581511fed1fe0cc2d2246dc77684e32dd4c4
n45567 <nea_30@mail.ru>
2025-09-29 22:34:30 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-using namespace std;
+#include <string>
 int main() {
-  int n;
-  cin >> n;
-  if (n == 1) {
-    cout << 500 << endl;
-  } else {
-    cout << 20 << endl;
-  }
-  return 0;
+    int n, s;
+    std::cin >> n;
+    std::string w1;
+    std::cin >> w1 >> s;
+    int median = 0;
+    for (int i = 0; i < s; i++) {
+        if (i > i + 1) {
+            std::swap (i; i + 1);
+        } else {
+            median == i - ((i - 1) / 2);
+        }
+    }
+    std::cout << median << std::endl;
+    return 0;
 }
