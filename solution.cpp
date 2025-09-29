1e194c9f70ae5b65977951946972c86295fb1a47
n45567 <nea_30@mail.ru>
2025-09-29 20:15:43 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -7,12 +7,15 @@ int main() {
     int total = 0;
     int count = 0;
     for (int i = 0; i < n; i++) {
-        int k, p;
-        std::string w1, w2, w3;
-        if (k == 1) {
-            std::cin >> w1 >> p >> w2;
-        } else {
-            std::cin >> w1 >> p >> w2 >> k >> w3;
+        int k, p; 
+        std::string word1, word2, word3, word4;
+        while (true) {
+            std::cin >> word1 >> word2 >> word3 >> word4;
+            if (word2 == "за" || k == 1) {
+                std::cin >> word1 >> word2 >> p >> word3; 
+            } else if (word2 == "по" || k > 1) {
+                std::cin >> word1 >> word2 >> p >> word3 >> k >> word4;  
+            }
         }
         total += k * p;
         count += k;
