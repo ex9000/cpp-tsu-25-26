62da94cdce529c6d12d420f135232b7776fd1565
VarenikiSVishney <son4ka2005@gmail.com>
2025-10-03 21:00:42 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void bubble_sort(vector<int>& arr){
+    
+    for(int i = 0; i < arr.size() - 1; i++){
+        for(int j = 0; j < arr.size() - i - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main(){
+    vector<int> words;
+    string line;
+    
+    getline(cin, line);
+    int n = stoi(line);
+    
+    for(int i = 0; i < n; i++){
+        getline(cin, line);
+        
+        string word;
+        string lastWord;
+        
+        istringstream lineStream(line);
+        
+        while(lineStream >> word){
+            lastWord = word;
+        }
+       words.push_back(stoi(lastWord)); 
+    }
+    bubble_sort(words);
+    
+    int res = words[n / 2];
+    cout << res << endl;
+    
+    return 0;
 }
