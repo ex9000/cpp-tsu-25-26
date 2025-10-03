3130f95d9e913a9612105af27793cdd0b3fc82bf
n45567 <nea_30@mail.ru>
2025-10-03 15:18:05 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <string>
 int main() {
-    int n, s;
+    int n;
     std::cin >> n;
-    std::string w1;
-    std::cin >> w1 >> s;
-    int median = 0;
-    for (int i = 0; i < s; i++) {
-        if (i > i + 1) {
-            std::swap (i; i + 1);
-        } else {
-            median == i - ((i - 1) / 2);
+    std::cin.ignore();  
+    long long* salaries = new long long[n];
+    for (int i = 0; i < n; ++i) {
+        std::string line;
+        std::getline(std::cin, line);
+        size_t pos = line.find_last_of(' ');
+        if (pos != std::string::npos) {
+            salaries[i] = std::stoll(line.substr(pos + 1));
         }
     }
-    std::cout << median << std::endl;
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (salaries[j] > salaries[j + 1]) {
+                long long temp = salaries[j];
+                salaries[j] = salaries[j + 1];
+                salaries[j + 1] = temp;
+            }
+        }
+    }
+    int median_index = (n - 1) / 2;
+    std::cout << salaries[median_index] << std::endl;
+    delete[] salaries;  
     return 0;
 }
