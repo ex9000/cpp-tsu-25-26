8719eca487e13a2585e49624af7ba9774f850f83
Vasilieva-Elena <elenademidova8487@gmail.com>
2025-10-10 19:45:12 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+void doSorting(long long a[], int num) {
+    for (int i = 0; i < num - 1; i++) {
+        for (int k = 0; k < num - i - 1; k++) {
+            if (a[k] > a[k + 1]) {
+                long long swap_val = a[k];
+                a[k] = a[k + 1];
+                a[k + 1] = swap_val;
+            }
+        }
+    }
+}
+
 int main() {
-  int n;
-  cin >> n;
-  if (n == 1) {
-    cout << 500 << endl;
-  } else {
-    cout << 20 << endl;
-  }
-  return 0;
+    int total;
+    cin >> total;
+    
+    long long *values = new long long[total];
+    
+    for (int idx = 0; idx < total; idx++) {
+        string text_line;
+        getline(cin >> ws, text_line);
+        
+        int last_space_pos = text_line.find_last_of(' ');
+        string number_str = text_line.substr(last_space_pos + 1);
+        values[idx] = stoll(number_str);
+    }
+    
+    doSorting(values, total);
+    
+    int center_pos = (total - 1) / 2;
+    cout << values[center_pos] << endl;
+    
+    delete[] values;
+    return 0;
 }
