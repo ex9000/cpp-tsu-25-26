2d038129f80abdfa03692271ceb3e9a441989f5f
Vasilieva-Elena <elenademidova8487@gmail.com>
2025-10-10 16:46:35 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,48 @@
 #include <iostream>
+
+using namespace std;
+
 int main() {
-  std::cout << "Куплен товар за 100 рублей" << std::endl;
-  return 0;
+  
+    char c;
+    while (cin >> c) {
+        if (c == ':') break;
+    }
+    int items;
+    cin >> items;
+    
+    int total_count = 0;
+    int total_amount = 0;
+    
+    for (int i = 0; i < items; i++) {
+        string current_word;
+        while (cin >> current_word) {
+            if (current_word == "за" || current_word == "по") {
+                break;
+            }
+        }
+        
+        int quantity, price;
+        cin >> price;
+        
+        if (current_word == "за") {
+            quantity = 1;
+            cin >> current_word;
+        } else {
+            cin >> current_word;
+            cin >> quantity;
+            cin >> current_word;
+        }
+        
+        total_count += quantity;
+        total_amount += quantity * price;
+    }
+  
+    if (total_count == 1) {
+        cout << "Куплен товар за " << total_amount << " рублей" << endl;
+    } else {
+        cout << "Куплено " << total_count << " штук товара за " << total_amount << " рублей" << endl;
+    }
+    
+    return 0;
 }
