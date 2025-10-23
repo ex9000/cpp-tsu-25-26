f6845f5e4cc1b8e07119564a89b8e7fd292f95bd
zoya-chizhova <chizhovazoya06@gmail.com>
2025-10-23 22:32:02 +0300

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,3 +1,5 @@
+struct Index { long long int index; double value; };
+
 void bubble_sort(Index arr[], long long int n){
     for(long long int i = 0; i < (n - 1); i++){
         for(long long int j = 0; j < (n - i - 1); j++){
