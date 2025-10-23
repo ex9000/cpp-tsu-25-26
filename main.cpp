a94bb4907261f110900e50075b28e1db126ebc72
zoya-chizhova <chizhovazoya06@gmail.com>
2025-10-23 22:27:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ struct Product {
 struct ScoredProduct { std::string name; double score; };
 struct Index { long long int index; double value; };
 
+void read_data(Product[] products, long long int n, long long int k);
+ScoredProduct convert_product(Product p, long long int k);
+void bubble_sort(Index arr[], long long int n);
+double sum(double arr[], long long int n);
+
 int main() {
     long long int n,k;
     cin>>n;
@@ -35,6 +40,6 @@ int main() {
     double total_score = sum(scores, 21);
     
     cout<<"Total score: "<<total_score<<endl;
-    cout<<"Best product: "<<best_products[20].name<<"(score: "<<best_products[20].score<<")"<<endl;
+    cout<<"Best product: "<<best_products[20].name<<"(score: "<<best_products[20].score<<")"<<endl; //в данном варианте не обязательно искать наилучший продукт, так как это будет последний продукт в отсортированном списке, поскольку его значение ближе всего к сумме
     return 0;
 }
