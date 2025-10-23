fd1ae0fdd2cff05687685a95af4fc88020f471e0
UnkindledAsh501 <jedisinobi@gmail.com>
2025-10-23 23:01:23 +0300

    Delete geometric.cpp

--- geometric.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-double geometric(double arr[], long long int n) {
-    double prod = 1.0;
-    for (long long int i = 0; i < n; i++) prod *= arr[i];
-    return pow(prod, 1.0 / n);
-}
