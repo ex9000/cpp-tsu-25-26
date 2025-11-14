2dbfe536c48f44c73057c3800d04165198d29555
Vasiliev-20 <fjbhgyuh12@gmail.com>
2025-11-14 13:36:15 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,43 +3,68 @@
 
 using namespace std;
 
-struct List {
-    // определите самостоятельно
+struct Node {
+    long long value;
+    Node* next;
+    Node(long long val) : value(val), next(nullptr) {}
 };
 
-int main() {
-    // Подсказка - используйте List
-    // А для создания новых элементов списка воспользуйтесь оператором new
+struct List {
+    Node* head;
+    Node* current;
 
-    // Тесты из примера можно пройти без списка, сохраняя только последние значения
-    // Но финальные тесты так не пройдут!
-    long long int last_value = 100;
-    long long int current_value = 100;
+    List() {
+        head = new Node(100);
+        current = head;
+    }
 
-    string cmd;
-    while (cin >> cmd) {
-        // Выполните обработку команд append, go, print, reset
-        if (cmd == "append") {
-            long long X; cin >> X;
-            // Ваша реализация здесь
-            last_value = current_value = X;
-            cout << "append " << X << " - OK" << '\n';
-        } else if (cmd == "go") {
-            long long N; cin >> N; long long originalN = N;
-            // Ваша реализация здесь
-            current_value = last_value;
-            cout << "go " << originalN << " - OK" << '\n';
-        } else if (cmd == "print") {
-            // Ваша реализация здесь
-            cout << "print " << current_value << " - OK" << '\n';
-        } else if (cmd == "reset") {
-            // Ваша реализация здесь
-            current_value = 100;
-            cout << "reset - OK" << '\n';
-        } else {
-            // Игнорируем неизвестные команды (не должны встречаться по условию)
+    void append(long long X) {
+        Node* newNode = new Node(X);
+        newNode->next = current->next;
+        current->next = newNode;
+        current = newNode;
+    }
+
+    void go(long long N) {
+        while (N > 0 && current->next != nullptr) {
+            current = current->next;
+            N--;
         }
     }
 
+    void reset() {
+        current = head;
+    }
+
+    void print() {
+        cout << "print " << current->value << " - OK" << endl;
+    }
+};
+
+int main() {
+    List list;
+    string cmd;
+
+    while (cin >> cmd) {
+        if (cmd == "append") {
+            long long X;
+            cin >> X;
+            list.append(X);
+            cout << "append " << X << " - OK" << endl;
+        } else if (cmd == "go") {
+            long long N;
+            cin >> N;
+            list.go(N);
+            cout << "go " << N << " - OK" << endl;
+        } else if (cmd == "print") {
+            list.print();
+        } else if (cmd == "reset") {
+            list.reset();
+            cout << "reset - OK" << endl;
+        }
+    }
+
+    return 0;
+}
     return 0;
 }
