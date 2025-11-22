3b49b7f92b3932d36be14a3096327f52f2a82098
Mariarty-pixel <mariapavlova715@gmail.com>
2025-11-22 23:20:41 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -19,6 +19,15 @@ struct List {
         current = head;
     }
     
+    ~List() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            Node* next = temp->next;
+            delete temp;
+            temp = next;
+        }
+    }
+    
     void append(long long X) {
         Node* newNode = new Node(X);
         newNode->next = current->next;
@@ -44,23 +53,30 @@ struct List {
 int main() {
     // Подсказка - используйте List
     List list;
+    // А для создания новых элементов списка воспользуйтесь оператором new
 
     string cmd;
     while (cin >> cmd) {
+        // Выполните обработку команд append, go, print, reset
         if (cmd == "append") {
             long long X; cin >> X;
+            // Ваша реализация здесь
             list.append(X);
             cout << "append " << X << " - OK" << '\n';
         } else if (cmd == "go") {
             long long N; cin >> N;
+            // Ваша реализация здесь
             list.go(N);
             cout << "go " << N << " - OK" << '\n';
         } else if (cmd == "print") {
-            long long value = list.print();
-            cout << "print " << value << " - OK" << '\n';
+            // Ваша реализация здесь
+            cout << "print " << list.print() << " - OK" << '\n';
         } else if (cmd == "reset") {
+            // Ваша реализация здесь
             list.reset();
             cout << "reset - OK" << '\n';
+        } else {
+            // Игнорируем неизвестные команды (не должны встречаться по условию)
         }
     }
 
